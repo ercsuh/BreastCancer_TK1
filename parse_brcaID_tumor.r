2d906a2c4884ffcafa8430f47b7d95dee2ff6c85
ercsuh <ekjsuh@gmail.com>
2018-05-15 14:39:36 -0600

    cleaned

--- parse_brcaID_tumor.r
+++ parse_brcaID_tumor.r
@@ -1,13 +1,13 @@
 require(readr)
-library(stringr)
-library(dplyr)
 
 # Read in file as data frame
-brca_patients <- as.data.frame(read_tsv('~/Documents/breastcancer/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt'))
+brca_IDs <- as.data.frame(read_tsv('~/Documents/BreastCancer_TK1/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt'))
 
-colnames(brca_patients) <- c("Patient", "Cancer")
+# Name columns
+colnames(brca_IDs) <- c("Patient", "CancerType")
 
-#brca_patients[which(brca_patients$Cancer == "BRCA"),]
-brca_patients <- brca_patients[grep("BRCA", brca_patients$Cancer), ]
+# Find patient IDs with Breast Cancer ("BRCA")
+brca_IDs <- brca_IDs[grep("BRCA", brca_IDs$Cancer), ]
 
-write.table(brca_patients, '~/Documents/breastcancer/brca_patients.tsv', quote = FALSE, sep = "\t", row.names = FALSE)
+# Write to a new file
+write.table(brca_IDs, '~/Documents/BreastCancer_TK1/brca_IDs.tsv', quote = FALSE, sep = "\t", row.names = FALSE)
